5cf815758532c7e2ac3e6c4ea4e105282efdbde3
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2025-12-26 18:46:18 +0100

    fix: remove tracing

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,6 @@ use axum::{
     Json, Router,
 };
 
-use tower_http::trace::TraceLayer;
-use tracing::Level;
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
-
 use diesel::r2d2::{self, ConnectionManager, Pool};
 use diesel::PgConnection;
 use dotenvy::dotenv;
@@ -91,16 +87,6 @@ async fn main() {
         println!("Bucket '{}' already exists.", bucket_name);
     }
 
-    // Init tracing
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
-            }),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = r2d2::Pool::builder()
@@ -130,7 +116,9 @@ async fn main() {
         .route("/api/message/{id}", post(api_handlers::get_one_message))
         .route("/api/message", post(api_handlers::upload_message))
         //.route("/api/anonymous/message/{id}/content", post(api_handlers::anonymous_message_get_content))
-        .layer(middleware::from_fn(api_handlers::jwt_auth)) // Apply JWT auth middleware to all routes above
+        .layer(middleware::from_fn(api_handlers::jwt_auth))
+        // Apply JWT auth middleware to all routes defined before this line
+
         .route("/api", get(api_handlers::root))
         .route("/api/register/start", post(api_handlers::register_user_start))
         .route("/api/register/end", post(api_handlers::register_user_end))
@@ -145,53 +133,9 @@ async fn main() {
         .route("/api/anonymous/message/{id}", get(api_handlers::anonymous_message_get_download_url))
         .with_state(state)
         .layer(DefaultBodyLimit::max(consts::MAX_BODY_SIZE))
-        .layer(cors)
-        .layer(middleware::from_fn(print_request_response));
+        .layer(cors);
 
     println!("Server running on {}", consts::URL);
     let listener = tokio::net::TcpListener::bind(consts::URL).await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn print_request_response(
-    req: Request,
-    next: Next,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-
-    tracing::debug!("Request URL: {}", req.uri()); // Log the URL
-
-    let (parts, body) = req.into_parts();
-    let bytes = buffer_and_print("request", body).await?;
-    let req = Request::from_parts(parts, Body::from(bytes));
-
-    let res = next.run(req).await;
-
-    let (parts, body) = res.into_parts();
-    let bytes = buffer_and_print("response", body).await?;
-    let res = Response::from_parts(parts, Body::from(bytes));
-
-    Ok(res)
-}
-
-async fn buffer_and_print<B>(direction: &str, body: B) -> Result<Bytes, (StatusCode, String)>
-where
-    B: axum::body::HttpBody<Data = Bytes>,
-    B::Error: std::fmt::Display,
-{
-
-    let bytes = match body.collect().await {
-        Ok(collected) => collected.to_bytes(),
-        Err(err) => {
-            return Err((
-                StatusCode::BAD_REQUEST,
-                format!("failed to read {direction} body: {err}"),
-            ));
-        }
-    };
-
-    if let Ok(body) = std::str::from_utf8(&bytes) {
-        tracing::debug!("{direction} body = {body:?}");
-    }
-
-    Ok(bytes)
 }
